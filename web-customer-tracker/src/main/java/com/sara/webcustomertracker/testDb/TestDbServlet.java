981e3316052afaeaa836572b1e89303b0e20ce64
saraHH1417 <sara.afzar@gmail.com>
2022-11-12 03:13:48 +0330

    feat: test jdbc, it is not correct yet

---
+++ web-customer-tracker/src/main/java/com/sara/webcustomertracker/testDb/TestDbServlet.java
@@ -0,0 +1,39 @@
+package com.sara.webcustomertracker.testDb;
+
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.PrintWriter;
+import java.sql.Connection;
+import java.sql.DriverManager;
+
+@WebServlet("/test-db-servlet")
+public class TestDbServlet extends HttpServlet {
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException {
+
+        String user = "springStudent";
+        String password = "springStudent";
+
+        String jdbcUrl = "jdbc:mysql//localhost:3306/web_customer_tracker?useSSL=false";
+        String driver = "com.mysql.cj.jdbc.Driver";
+
+        try {
+            PrintWriter out = response.getWriter();
+            System.out.println("connecting to database: " + jdbcUrl);
+//            Class.forName(driver);
+            Connection myConn = DriverManager.getConnection(jdbcUrl, user, password);
+
+            System.out.println("Success!!!");
+            myConn.close();
+        }catch (Exception exception) {
+            exception.printStackTrace();
+            throw new ServletException(exception);
+        }
+
+    }
+}
