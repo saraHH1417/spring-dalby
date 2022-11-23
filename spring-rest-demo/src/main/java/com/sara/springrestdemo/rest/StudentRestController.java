532b392a6f551f5d5465aed08e0c5becb97ffa35
saraHH1417 <sara.afzar@gmail.com>
2022-11-24 00:23:08 +0330

    feat: get one student

--- spring-rest-demo/src/main/java/com/sara/springrestdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/sara/springrestdemo/rest/StudentRestController.java
@@ -2,23 +2,34 @@ package com.sara.springrestdemo.rest;
 
 import com.sara.springrestdemo.entity.Student;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.annotation.PostConstruct;
 import java.util.ArrayList;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
+    private List<Student> theStudents;
 
-    @GetMapping("/students")
-    public List<Student> getStudents() {
-        List<Student> theStudents = new ArrayList<>();
+    @PostConstruct
+    public void loadData()  {
+        theStudents = new ArrayList<>();
         theStudents.add(new Student("sara", "gfg"));
         theStudents.add(new Student("mary" , "blue"));
         theStudents.add(new Student("john", "grey"));
+    }
 
+    @GetMapping("/students")
+    public List<Student> getStudents() {
         return theStudents;
+    }
+
+    @GetMapping("/students/{studentId}")
+    public Student getStudent(@PathVariable int studentId) {
+        return theStudents.get(studentId);
     }
 }
