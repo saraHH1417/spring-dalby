2bb11a6701d55870280d4e2c0ccf2b42c7d15a89
saraHH1417 <sara.afzar@gmail.com>
2022-11-26 01:32:02 +0330

    feat: update and delete customer

--- spring-crm-rest/src/main/java/com/sara/springcrmrest/rest/CustomerRestController.java
+++ spring-crm-rest/src/main/java/com/sara/springcrmrest/rest/CustomerRestController.java
@@ -3,9 +3,12 @@ package com.sara.springcrmrest.rest;
 import com.sara.springcrmrest.entity.Customer;
 import com.sara.springcrmrest.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequestMapping("/api")
@@ -40,6 +43,23 @@ public class CustomerRestController {
     public Customer updateCustomer(@RequestBody Customer customer) {
         customerService.saveCustomer(customer);
         return customer;
+    }
+
+
+    @DeleteMapping("/customers/{customerId}")
+    public Map<String, String> deleteCustomer(@PathVariable int customerId) {
+        Customer customer = customerService.getCustomer(customerId);
+
+        if (customer == null) {
+            throw  new CustomerNotFoundException("Customer with id " + customerId + " not found.");
+        }
+
+        customerService.deleteCustomer(customerId);
+
+        Map<String, String> result = new HashMap<>();
+        result.put("message" , "Successfully deleted customer with id " + customerId);
+        result.put("status", HttpStatus.ACCEPTED.toString());
+        return result;
     }
 
 }
