fb1211306dbb0e84bbcb5a94f156dfa083712b30
saraHH1417 <sara.afzar@gmail.com>
2022-11-30 02:44:51 +0330

    feat: changing application context properties

--- spring-boot-dev-tools/src/main/java/com/sara/springbootdevtools/rest/FunRestController.java
+++ spring-boot-dev-tools/src/main/java/com/sara/springbootdevtools/rest/FunRestController.java
@@ -1,5 +1,6 @@
 package com.sara.springbootdevtools.rest;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,6 +8,19 @@ import java.time.LocalDateTime;
 
 @RestController
 public class FunRestController {
+
+    // inject properties for coach.name and team.nane
+    @Value("${coach.name}")
+    private String coachName;
+
+    @Value("${team.name}")
+    private String teamName;
+
+    @GetMapping("/teaminfo")
+    public String getTeamInfo() {
+        return "Coach: " + coachName + " ;Team name: " + coachName;
+    }
+
 
     @GetMapping("/")
     public String sayHello() {
