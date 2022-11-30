cacb5cbb05d979db98b319bfd3a1c690acde514f
saraHH1417 <sara.afzar@gmail.com>
2022-12-01 01:49:43 +0330

    feat: spring data rest and it's config

--- boot-crud-demo/src/main/java/com/sara/bootcruddemo/dao/EmployeeRepository.java
+++ boot-crud-demo/src/main/java/com/sara/bootcruddemo/dao/EmployeeRepository.java
@@ -2,7 +2,10 @@ package com.sara.bootcruddemo.dao;
 
 import com.sara.bootcruddemo.entity.Employee;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
+import org.springframework.stereotype.Repository;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
