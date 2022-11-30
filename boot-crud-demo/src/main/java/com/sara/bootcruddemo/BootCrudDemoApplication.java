2e9e0c622331884c7f615075c899783154726b96
saraHH1417 <sara.afzar@gmail.com>
2022-11-30 03:40:28 +0330

    feat: setting database properties and creating entity

---
+++ boot-crud-demo/src/main/java/com/sara/bootcruddemo/BootCrudDemoApplication.java
@@ -0,0 +1,13 @@
+package com.sara.bootcruddemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootCrudDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootCrudDemoApplication.class, args);
+	}
+
+}
---
+++ boot-crud-demo/src/test/java/com/sara/bootcruddemo/BootCrudDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sara.bootcruddemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootCrudDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
