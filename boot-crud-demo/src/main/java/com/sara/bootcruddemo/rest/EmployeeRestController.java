876536d863127a1af7673aa9cdc01773d92a4557
saraHH1416 <z.haghighi19@gmail.com>
2022-11-30 18:47:01 +0330

    feat: add update and delte methods to restcontroller

--- boot-crud-demo/src/main/java/com/sara/bootcruddemo/rest/EmployeeRestController.java
+++ boot-crud-demo/src/main/java/com/sara/bootcruddemo/rest/EmployeeRestController.java
@@ -2,28 +2,66 @@ package com.sara.bootcruddemo.rest;
 
 import com.sara.bootcruddemo.dao.EmployeeDAO;
 import com.sara.bootcruddemo.entity.Employee;
+import com.sara.bootcruddemo.service.EmployeeService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class EmployeeRestController {
-    private EmployeeDAO employeeDAO;
+    private EmployeeService employeeService;
 
     // quick and dirty: inject employee dao (using constructor injection)
     @Autowired
-    public EmployeeRestController(EmployeeDAO theEmployeeDAO) {
-        employeeDAO = theEmployeeDAO;
+    public EmployeeRestController(EmployeeService theEmployeeService) {
+        employeeService = theEmployeeService;
     }
 
     @GetMapping("/employees")
     public List<Employee> findAll() {
-        return  employeeDAO.findAll();
+        return  employeeService.findAll();
     }
 
+    @GetMapping("/employees/{employeeId}")
+    public Employee getEmployee(@PathVariable int employeeId) {
+        Employee theEmployee = employeeService.findById(employeeId);
 
+        if(theEmployee == null) {
+           throw new RuntimeException("Employee with id " + employeeId + " not found.");
+        }
+
+        return theEmployee;
+    }
+
+    @PostMapping("/employees")
+    public Employee addEmployee(@RequestBody Employee theEmployee) {
+        // we put below line because it is possible that they pass an id in the request
+        theEmployee.setId(0);
+
+        employeeService.save(theEmployee);
+
+        return  theEmployee;
+    }
+
+    @PutMapping("/employees")
+    public Employee updateEmployee(@RequestBody Employee theEmployee) {
+        employeeService.save(theEmployee);
+
+        return  theEmployee;
+    }
+
+    @DeleteMapping("/employees/{employeeId}")
+    public String deleteEmployee(@PathVariable int employeeId) {
+        Employee tempEmployee = employeeService.findById(employeeId);
+
+        if(tempEmployee == null) {
+            throw new RuntimeException("Employee with id " + employeeId + " not found.");
+        }
+
+        employeeService.deleteById(employeeId);
+
+        return  "Delete Employee id - " + employeeId;
+    }
 }
---
+++ boot-crud-demo/src/main/java/com/sara/bootcruddemo/service/EmployeeServiceImpl.java
@@ -0,0 +1,46 @@
+package com.sara.bootcruddemo.service;
+
+
+import com.sara.bootcruddemo.dao.EmployeeDAO;
+import com.sara.bootcruddemo.entity.Employee;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+public class EmployeeServiceImpl implements EmployeeService{
+
+    public EmployeeDAO employeeDAO;
+
+    @Autowired
+    public EmployeeServiceImpl(EmployeeDAO theEmployeeDao) {
+        employeeDAO = theEmployeeDao;
+    }
+
+
+    @Override
+    @Transactional
+    public List<Employee> findAll() {
+        return employeeDAO.findAll();
+    }
+
+    @Override
+    @Transactional
+    public Employee findById(int theId) {
+        return employeeDAO.findById(theId);
+    }
+
+    @Override
+    @Transactional
+    public void save(Employee theEmployee) {
+        employeeDAO.save(theEmployee);
+    }
+
+    @Override
+    @Transactional
+    public void deleteById(int theId) {
+        employeeDAO.deleteById(theId);
+    }
+}
